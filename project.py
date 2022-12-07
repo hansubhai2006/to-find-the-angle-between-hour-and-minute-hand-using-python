fe36665332629eeaf2566090521366330e857542
Hansraj <119502496+hansubhai2006@users.noreply.github.com>
2022-12-07 12:54:01 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,25 @@
+# Function to compute the angle between the hour and minute hand
+def findAngle(hh, mm):
+    # handle 24-hour notation
+    hh = hh % 12
+
+    # find the position of the hour's hand
+    h = (hh * 360) // 12 + (mm * 360) // (12 * 60)
+
+    # find the position of the minute's hand
+    m = (mm * 360) // (60)
+
+    # calculate the angle difference
+    angle = abs(h - m)
+
+    # consider the shorter angle and return it
+    if angle > 180:
+        angle = 360 - angle
+
+    return angle
+
+
+# Clock Angle Problem
+hh=int(input("Enter hour:"))
+mm=int(input("Enter minute:"))
+print("Angle between hour and minute hand is:",findAngle(hh,mm))
